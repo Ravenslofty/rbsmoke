56aed4cd6d3dacf22829016731fc806538bf11b4
Dan Ravensloft <dan.ravensloft@gmail.com>
2017-08-14 12:18:59 +0100

    Command-line arguments

--- main.go
+++ main.go
@@ -1,14 +1,37 @@
 package main
 
 import (
+    "flag"
     "fmt"
+    "log"
     "math"
+    "os"
+    "runtime"
+    "runtime/pprof"
 )
 
+var width = flag.Int("w", 64, "Rendered image width")
+var height = flag.Int("h", 64, "Rendered image height")
+
+var cpuprofile = flag.String("cpuprofile", "", "write cpu profile `file`")
+var memprofile = flag.String("memprofile", "", "write memory profile to `file`")
+
 func main() {
-    // TODO: make these configurable.
-    x_size := 64
-    y_size := 64
+    flag.Parse()
+
+    if *cpuprofile != "" {
+        f, err := os.Create(*cpuprofile)
+        if err != nil {
+            log.Fatal("could not create CPU profile: ", err)
+        }
+        if err := pprof.StartCPUProfile(f); err != nil {
+            log.Fatal("could not start CPU profile: ", err)
+        }
+        defer pprof.StopCPUProfile()
+    }
+
+    x_size := *width
+    y_size := *height
 
     // Since we have a single pixel per image, we approximate the number
     // of colours per channel.
@@ -18,4 +41,16 @@ func main() {
                 x_size, y_size, colours)
 
     Render(x_size, y_size, colours)
+
+    if *memprofile != "" {
+        f, err := os.Create(*memprofile)
+        if err != nil {
+            log.Fatal("could not create memory profile: ", err)
+        }
+        runtime.GC() // get up-to-date statistics
+        if err := pprof.WriteHeapProfile(f); err != nil {
+            log.Fatal("could not write memory profile: ", err)
+        }
+        f.Close()
+    }
 }
