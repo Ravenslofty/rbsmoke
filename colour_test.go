0ea8661a0b06b231c4e80ffe088ca08e4e1d0429
Dan Ravensloft <dan.ravensloft@gmail.com>
2017-08-23 11:45:32 +0100

    fight compiler optimisations

--- colour_test.go
+++ colour_test.go
@@ -8,39 +8,33 @@ import (
 
 func RandomColour() (a color.NRGBA) {
     x := rand.Uint32()
-
     a = color.NRGBA{uint8(x & 255), uint8((x >> 8) & 255), uint8((x >> 16) & 255), 255}
     return
 }
 
+var x, y color.NRGBA = RandomColour(), RandomColour()
+var bench_result int32
+
 func BenchmarkColourDiffRgb(b *testing.B) {
-    rand.Seed(1)
-
-    x, y := RandomColour(), RandomColour()
-
+    var r int32
     for n := 0; n < b.N; n++ {
-        ColourDiffRgb(x, y)
+        r = ColourDiffRgb(x, y)
     }
+    bench_result = r
 }
 
 func BenchmarkColourDiffLab(b *testing.B) {
-    rand.Seed(1)
-
-    x, y := RandomColour(), RandomColour()
-
+    var r int32
     for n := 0; n < b.N; n++ {
-        ColourDiffLab(x, y)
+        r = ColourDiffLab(x, y)
     }
+    bench_result = r
 }
 
 func BenchmarkColourDiffLabNoConversion(b *testing.B) {
-    rand.Seed(1)
-
-    x, y := MakeColorful(RandomColour()), MakeColorful(RandomColour())
-
+    _x, _y := MakeColorful(x), MakeColorful(y)
     for n := 0; n < b.N; n++ {
-        x.DistanceLab(y)
+        _x.DistanceLab(_y)
     }
 }
-
 
